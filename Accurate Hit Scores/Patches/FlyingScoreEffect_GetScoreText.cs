d9409475a3f70e5a135fe9ac3fadc66ee5f22bc2
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-11-18 13:16:49 -0600

    Made the whole mod.

---
+++ Accurate Hit Scores/Patches/FlyingScoreEffect_GetScoreText.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using UnityEngine;
+using System.Linq;
+
+namespace Accurate_Hit_Scores.Patches
+{
+    [HarmonyPatch(typeof(FlyingScoreEffect))]
+    [HarmonyPatch("GetScoreText")]
+    class FlyingScoreEffect_GetScoreText
+    {
+        private static bool Prefix(int score, ref string __result)
+        {
+            if (_scoreController == null) _scoreController = Resources.FindObjectsOfTypeAll<ScoreController>().FirstOrDefault();
+            __result = (score * _scoreController.multiplierWithFever).ToString(); 
+
+            return false;
+        }
+
+        private static ScoreController _scoreController;
+    }
+}
--- Accurate Hit Scores/Plugin.cs
+++ Accurate Hit Scores/Plugin.cs
@@ -1,4 +1,7 @@
-﻿using IPA;
+﻿using CustomUI.GameplaySettings;
+using Harmony;
+using IPA;
+using System.Reflection;
 using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
@@ -6,12 +9,19 @@ namespace Accurate_Hit_Scores
 {
     public class Plugin : IBeatSaberPlugin
     {
-        public void Init(IPALogger logger)
-        {
-            Logger.logger = logger;
-        }
+        private static bool isEnabled;
+        private static HarmonyInstance harmony;
+        private static readonly string id = "com.steven.accurate.hit.scores";
+        private static BS_Utils.Utilities.Config config;
 
-        public void OnApplicationStart() { }
+        public void Init(IPALogger logger) => Logger.logger = logger;
+
+        public void OnApplicationStart()
+        {
+            harmony = HarmonyInstance.Create(id);
+            config = new BS_Utils.Utilities.Config("Accurate Hit Scores");
+            isEnabled = config.GetBool("Settings", "Enabled", false, true);
+        }
 
         public void OnApplicationQuit() { }
 
@@ -19,10 +29,40 @@ namespace Accurate_Hit_Scores
 
         public void OnUpdate() { }
 
-        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) { }
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+            if (nextScene.name == "GameCore" && isEnabled)
+            {
+                Logger.Log("Applying Harmony Patches");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                if (harmony.HasAnyPatches(id))
+                {
+                    Logger.Log("Removing Harmony Patches");
+                    harmony.UnpatchAll(id);
+                }
+            }
+        }
 
-        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            if (scene.name == "MenuCore") CreateUI();
+        }
 
         public void OnSceneUnloaded(Scene scene) { }
+
+        public static void CreateUI()
+        {
+            var enableToggle = GameplaySettingsUI.CreateToggleOption(GameplaySettingsPanels.PlayerSettingsRight, "Accurate Hit Score", "Apply Combo to Hit Scores", null, 0);
+            enableToggle.GetValue = isEnabled;
+            enableToggle.OnToggle += (value) =>
+            {
+                isEnabled = value;
+                config.SetBool("Settings", "Enabled", value);
+            };
+        }
+
     }
 }
